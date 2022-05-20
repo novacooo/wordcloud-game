f806c679f598c0bb56a70b515ec35e2382ab7835
novacooo <jaceknowak226@gmail.com>
2022-05-21 00:22:21 +0200

    Implement fake api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,26 @@
 /* eslint-disable import/prefer-default-export */
 import database from './database';
 
-export const getSets = () => Object.values(database);
+const { questions } = database;
+
+export const getQuestions = () => {
+  return new Promise((resolve, reject) => {
+    if (!questions) {
+      setTimeout(() => reject(new Error('Questions not found')), 250);
+    }
+
+    setTimeout(() => resolve(Object.values(questions)), 250);
+  });
+};
+
+export const getQuestion = (id: string) => {
+  return new Promise((resolve, reject) => {
+    const question = questions[id];
+
+    if (!question) {
+      setTimeout(() => reject(new Error('Question not found')), 250);
+    }
+
+    setTimeout(() => resolve(question), 250);
+  });
+};
